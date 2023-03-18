aa1b465d04576e6d0f058eecf940282febc97003
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-03-18 01:52:07 -0500

    Added api functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,62 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+console.log('current directory:', __dirname);
+
+fs.mkdirSync('/app/macroStore/v0', { recursive: true });
+fs.writeFileSync('/app/macroStore/v0/test.json', '{"key": "value"}');
+
+// setting up fake file structure to show off api in structure
+fs.mkdirSync('/app/macroStore/api', { recursive: true });
+fs.writeFileSync('/app/macroStore/api/update', '');
+
+app.get('/api/update', (req, res) => {
+  if (!process.env.TOKEN) {
+    res.send('No token set in environment variable TOKEN');
+    return;
+  };
+
+  if (!req.headers['Auth-Token']) {
+    res.send('No Auth-Token header set');
+    return;
+  };
+
+  if (!req.query['path']) {
+    res.send('No path query parameter set');
+    return;
+  };
+
+  if (!req.body) {
+    res.send('No body set');
+    return;
+  }
+
+  const absPath = path.resolve(path.join('/app/macroStore/', req.query['path']));
+  let fileBody = '';
+
+  try {
+    fileBody = JSON.parse(req.body);
+  } catch (e) {
+    res.send('Body is not valid JSON');
+    return;
+  };
+
+  if (!absPath.startsWith('/app/macroStore/')) {
+    res.send('Path is not in macroStore');
+    return;
+  };
+
+  console.log('Update request received');
+  if (req.headers['Auth-Token'] === process.env.TOKEN) {
+    fs.writeFileSync(absPath, JSON.stringify(fileBody));
+  } else {
+    console.log('someone is being naughty');
+  }
+
+  res.send(absPath);
 });
 
 app.listen(3000, () => {
