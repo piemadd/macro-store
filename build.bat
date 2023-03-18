85f36de59c38fa1f5747cb67cb79fc6b61f10d37
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-03-18 02:05:07 -0500

    fixed config and build script

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ del build.json
 nixpacks plan -e PORT=8080 -p "nodejs-18_x nginx" -b "cd src && npm install && mkdir -p /var/log/nginx && touch /var/log/nginx/error.log" -s "node /app/src/replaceAuthToken.js && nginx -c /app/nginx/nginx.conf && node /app/src/index.js" . > build.json
 move /Y build.json nixpacks.json
 REM too lazy to have it read from json, live with me here
-nixpacks build -n macro-store -e PORT=8080 -p "nodejs-18_x nginx" -b "cd src && npm install && mkdir -p /var/log/nginx && touch /var/log/nginx/error.log" -s "node /app/src/replaceAuthToken.js && nginx -c /app/nginx/nginx.conf && node /app/src/index.js" -v .
+nixpacks build -n macro-store -e PORT=8080 -v -c nixpacks.json .
